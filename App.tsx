61730a6075c9cde1751f03a2457b0fcbc52e2c71
Julia <juliabcohen@icloud.com>
2022-03-28 22:52:29 -0400

    adcionei botão para levar ao menu

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font';
+import { StyleSheet, Text, View, Button, SafeAreaView, Alert } from 'react-native';
+import { useFonts } from 'expo-font'; 
 import AppLoading from 'expo-app-loading';
 
-export default function App() {
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+export default function App(): JSX.Element {
   let [fontsLoaded] = useFonts({
     'courier-prime': require('./assets/fonts/courier-prime.ttf'),
   });
@@ -14,6 +18,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Blue Side</Text>
+      <Separator />
+      <Button 
+      title="Click here to start" 
+      onPress={() => Alert.alert('take to menu') }
+      color="#438788" />
       <StatusBar style="auto"/>
     </View>
   );
@@ -31,5 +40,17 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontFamily: 'courier-prime',
     fontSize: 100,
-  }
+  },
+
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  separator: {
+    marginVertical: 100,
+    borderBottomColor: '#000000',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+
 });
